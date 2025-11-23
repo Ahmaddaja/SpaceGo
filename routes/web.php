c507f110d517f28bef1ffbbbac4d8923403a8d1b
Akbardwi123 <akbardwipebriansyah@gmail.com>
2025-11-23 14:09:25 +0700

    setelan awal

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\RakController;
-
 
 // Halaman awal
 Route::get('/', function () {
@@ -23,7 +21,6 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', function () {
         return view('admin.dashboard');
     })->name('admin.dashboard');
-    Route::resource('raks', RakController::class);
 });
 
 // Route Profile (untuk semua user yang login)
@@ -53,3 +50,5 @@ Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('
 
 // Proses Register
 Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+
+
