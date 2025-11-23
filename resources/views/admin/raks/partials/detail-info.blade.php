4f33763a74ff609f3ecb873271b0f30ef3165960
Akbardwi123 <akbardwipebriansyah@gmail.com>
2025-11-23 21:23:24 +0700

    menghapus status active

--- resources/views/admin/raks/partials/detail-info.blade.php
+++ resources/views/admin/raks/partials/detail-info.blade.php
@@ -43,14 +43,14 @@
                 <td>: <strong class="text-success">{{ $rak->harga_format }}</strong></td>
             </tr>
             <tr>
-                <td class="font-weight-bold">Status Aktif</td>
+                {{-- <td class="font-weight-bold">Status Aktif</td>
                 <td>: 
                     @if($rak->is_active)
                         <span class="badge badge-success">Aktif</span>
                     @else
                         <span class="badge badge-secondary">Tidak Aktif</span>
                     @endif
-                </td>
+                </td> --}}
             </tr>
         </table>
 
