e21d0612711c220501ebf601ac117b556422f11b
Akbardwi123 <akbardwipebriansyah@gmail.com>
2025-11-23 14:31:19 +0700

    web rote fix

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RakController;
 
 // Halaman awal
 Route::get('/', function () {
@@ -26,6 +27,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', function () {
         return view('admin.dashboard');
     })->name('admin.dashboard');
+    Route::resource('admin/customers', RakController::class);
 });
 
 // Route Profile (untuk semua user yang login)
