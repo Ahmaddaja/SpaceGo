ee0cda9e836697b04c907e940ff9f653f987e5d4
Quaryy <jonscgm0@gmail.com>
2025-11-25 02:54:46 +0700

    benerin eror di show raks

--- resources/views/admin/raks/show.blade.php
+++ resources/views/admin/raks/show.blade.php
@@ -7,17 +7,15 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-
-         <div class="col-lg-8">
+        <div class="col-lg-8">
             @include('admin.raks.partials.detail-info', ['rak' => $rak])
-            @include('admin.raks.partials.detail-rak-list', ['rak' => $rak])
+            @include('admin.raks.partials.detail-specifications', ['rak' => $rak])
         </div>
 
         <div class="col-lg-4">
             @include('admin.raks.partials.detail-photo', ['rak' => $rak])
             @include('admin.raks.partials.detail-actions', ['rak' => $rak])
         </div>
-
     </div>
 </div>
 @endsection
