311bea83989be618c0584b9590deb16d274d2c0b
RenVoO <ahmadfahrifauzan@gmail.com>
2025-11-24 10:54:29 +0700

    update customer content

--- resources/views/customer/list-rak/show.blade.php
+++ resources/views/customer/list-rak/show.blade.php
@@ -44,7 +44,7 @@
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                     </svg>
-                    <span class="text-xs mt-1 hidden md:block">History</span>
+                    <span class="text-xs mt-1 hidden md:block">Riwayat</span>
                 </a>
                 
                 <form action="{{ route('logout') }}" method="POST" class="inline">
